480e36371c0aadb2901666402c29752486ff49e6
Andrew Krause <andrew@nodeprime.com>
2014-12-16 17:59:37 -0800

    [UPDATE] Basic sparkline capability

--- src/js/spark-line.js
+++ src/js/spark-line.js
@@ -25,6 +25,12 @@
 (function() {
   'use strict';
 
+  // VARIABLES //
+  var // default width in pixels
+    WIDTH = 100,
+    // default height in pixels
+    HEIGHT = 25;
+
   // POLYMER //
 
   Polymer( 'spark-line', {
@@ -33,8 +39,67 @@
     *  Polymer hook that is called when an element is created.
     */
     created: function() {
-      console.log( this.data );
-      console.log( 'width:', this.width, 'height:', this.height );
+      this.data = [];
+    },
+
+    /**
+    * METHOD: domReady()
+    *  Polymer hook that is called when an element is ready for insertion into the DOM.
+    */
+    domReady: function() {
+      this.init();
+    },
+
+    /**
+    * METHOD: init()
+    *  Initialization
+    */
+    init: function() {
+      var create = document.createElement.bind( document ),
+        el,
+        d3,
+        width,
+        height,
+        x,
+        y,
+        line;
+
+      // Create a D3 element to access libray dependencies
+      el = create( 'polymer-d3' );
+      d3 = el.d3;
+      this._d3 = d3;
+
+      // Set dimensions
+      width = this.width ? this.width: WIDTH;
+      height = this.height ? this.height: HEIGHT;
+
+      // Create range
+      x = d3.scale
+        .linear()
+        .range( [ 0, width ] );
+      y = d3.scale
+        .linear()
+        .range( [ height, 0 ] );
+
+
+      // Create sparkline
+      line = d3.svg
+        .line()
+        .interpolate( 'basis' )
+        .x( function( d, i ) { return x( i ); } )
+        .y( function( d ) { return y( d ); } );
+
+      x.domain( [0, this.data.length] );
+      y.domain( d3.extent( this.data ) );
+
+      d3.select( this.$.sparkline )
+        .append( 'svg' )
+        .attr( 'width', width )
+        .attr( 'height', height )
+        .append( 'path' )
+        .datum( this.data )
+        .attr( 'class', 'sparkline' )
+        .attr( 'd', line );
     }
 
   });
